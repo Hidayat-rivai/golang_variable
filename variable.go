143433b912cb9e0fee9a5a2d426a74455208080b
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-12-20 19:39:19 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nama string = "Hidayat"
+	var namalain = "Jack"
+	namatengah := "Night"
+	
+	fmt.Printf("Halo %s %s %s", nama, namalain, namatengah)
+}
